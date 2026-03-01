2d09a053982f0e68f5058ec374bfbe5aa253ae10
bibekyogi6462 <bibekyogi6462@gmail.com>
2026-03-01 10:20:29 +0545

    Add initial Spring Boot project structure and update README

---
+++ src/main/java/com/bibek/enterprisepossystem/EnterprisepossystemApplication.java
@@ -0,0 +1,13 @@
+package com.bibek.enterprisepossystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterprisepossystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterprisepossystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibek/enterprisepossystem/EnterprisepossystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibek.enterprisepossystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterprisepossystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
